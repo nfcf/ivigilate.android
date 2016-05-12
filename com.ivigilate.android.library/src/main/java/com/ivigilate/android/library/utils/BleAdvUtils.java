a6aec331ba4554866c649329e6ac7d7f82da039f
nfcf <nunofcf@gmail.com>
2016-05-12 11:30:39 +0100

    fixed wrong manufacturer id for samsung

--- com.ivigilate.android.library/src/main/java/com/ivigilate/android/library/utils/BleAdvUtils.java
+++ com.ivigilate.android.library/src/main/java/com/ivigilate/android/library/utils/BleAdvUtils.java
@@ -51,7 +51,7 @@ public class BleAdvUtils {
     private static final int MANUFACTURER_GIMBAL = 0xa0c6; //"ad7700c6";
     private static final int MANUFACTURER_JABRA = 0x180d;
     private static final int MANUFACTURER_TI = 0xaa80;
-    private static final int MANUFACTURER_SAMSUNG = 0x7500;
+    private static final int MANUFACTURER_SAMSUNG = 0x0075;
     private static final int MANUFACTURER_PEBBLE = 0xfed9;
 
 }
